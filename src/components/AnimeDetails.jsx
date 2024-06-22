52ead2806f50a8fc16604b4339a8fbedd45c4384
Abdulrahman Ahmed <bedo150@gmail.com>
2024-06-22 08:47:38 +0300

    update AnimeDetails.jsx and Details.css

--- src/components/AnimeDetails.jsx
+++ src/components/AnimeDetails.jsx
@@ -5,85 +5,91 @@ const AnimeDetails = ({ anime }) => {
   return (
     <div className="anime-details">
       <h2 className="anime-title">{anime.title}</h2>
-      <img src={anime.images.jpg.image_url} alt={anime.title} className="detail-image" />
-      <p>
-        <span className="anime-details-ids">Type:</span>
-        <span className="anime-details-values">
-          {anime.type ? anime.type : <span className="unknown-details">unknown</span>}
-        </span>
-      </p>
-      <p>
-        <span className="anime-details-ids">Source:</span>
-        <span className="anime-details-values">
-          {anime.source ? anime.source : <span className="unknown-details">unknown</span>}
-        </span>
-      </p>
-      <p>
-        <span className="anime-details-ids">Score:</span>
-        <span className="anime-details-values">
-          {anime.score ? anime.score : <span className="unknown-details">unknown</span>}
-        </span>
-      </p>
-      <p>
-        <span className="anime-details-ids">Rank:</span>
-        <span className="anime-details-values">
-          {anime.rank ? anime.rank : <span className="unknown-details">unknown</span>}
-        </span>
-      </p>
-      <p>
-        <span className="anime-details-ids">Episodes:</span>
-        <span className="anime-details-values">
-          {anime.episodes ? anime.episodes : <span className="unknown-details">unknown</span>}
-        </span>
-      </p>
-      <p>
-        <span className="anime-details-ids">Genres:</span>
-        <span className="anime-details-values">
-          {anime.genres && anime.genres.length > 0 ? (
-            anime.genres.map((genre) => genre.name).join(", ")
-          ) : (
-            <span className="unknown-details">unknown</span>
-          )}
-        </span>
-      </p>
-      <p>
-        <span className="anime-details-ids">Producers:</span>
-        <span className="anime-details-values">
-          {anime.producers && anime.producers.length > 0 ? (
-            anime.producers[0].name
-          ) : (
-            <span className="unknown-details">unknown</span>
-          )}
-        </span>
-      </p>
-      <p>
-        <span className="anime-details-ids">Studios:</span>
-        <span className="anime-details-values">
-          {anime.studios && anime.studios.length > 0 ? (
-            anime.studios[0].name
-          ) : (
-            <span className="unknown-details">unknown</span>
-          )}
-        </span>
-      </p>
-      <p>
-        <span className="anime-details-ids">Year:</span>
-        <span className="anime-details-values">
-          {anime.year ? anime.year : <span className="unknown-details">unknown</span>}
-        </span>
-      </p>
-      <p>
-        <span className="anime-details-ids">Synopsis:</span>
-        <span className="anime-details-values">
-          {anime.synopsis ? anime.synopsis : <span className="unknown-details">unknown</span>}
-        </span>
-      </p>
-      <p>
-        <span className="anime-details-ids">Background:</span>
-        <span className="anime-details-values">
-          {anime.background ? anime.background : <span className="unknown-details">unknown</span>}
-        </span>
-      </p>
+      <div className="details-container">
+        <div className="left-side">
+          <img src={anime.images.jpg.image_url} alt={anime.title} className="detail-image" />
+        </div>
+        <div className="right-side">
+          <p>
+            <span className="anime-details-ids">Type:</span>
+            <span className="anime-details-values">
+              {anime.type ? anime.type : <span className="unknown-details">unknown</span>}
+            </span>
+          </p>
+          <p>
+            <span className="anime-details-ids">Source:</span>
+            <span className="anime-details-values">
+              {anime.source ? anime.source : <span className="unknown-details">unknown</span>}
+            </span>
+          </p>
+          <p>
+            <span className="anime-details-ids">Score:</span>
+            <span className="anime-details-values">
+              {anime.score ? anime.score : <span className="unknown-details">unknown</span>}
+            </span>
+          </p>
+          <p>
+            <span className="anime-details-ids">Rank:</span>
+            <span className="anime-details-values">
+              {anime.rank ? anime.rank : <span className="unknown-details">unknown</span>}
+            </span>
+          </p>
+          <p>
+            <span className="anime-details-ids">Episodes:</span>
+            <span className="anime-details-values">
+              {anime.episodes ? anime.episodes : <span className="unknown-details">unknown</span>}
+            </span>
+          </p>
+          <p>
+            <span className="anime-details-ids">Genres:</span>
+            <span className="anime-details-values">
+              {anime.genres && anime.genres.length > 0 ? (
+                anime.genres.map((genre) => genre.name).join(", ")
+              ) : (
+                <span className="unknown-details">unknown</span>
+              )}
+            </span>
+          </p>
+          <p>
+            <span className="anime-details-ids">Producers:</span>
+            <span className="anime-details-values">
+              {anime.producers && anime.producers.length > 0 ? (
+                anime.producers[0].name
+              ) : (
+                <span className="unknown-details">unknown</span>
+              )}
+            </span>
+          </p>
+          <p>
+            <span className="anime-details-ids">Studios:</span>
+            <span className="anime-details-values">
+              {anime.studios && anime.studios.length > 0 ? (
+                anime.studios[0].name
+              ) : (
+                <span className="unknown-details">unknown</span>
+              )}
+            </span>
+          </p>
+          <p>
+            <span className="anime-details-ids">Year:</span>
+            <span className="anime-details-values">
+              {anime.year ? anime.year : <span className="unknown-details">unknown</span>}
+            </span>
+          </p>
+          <p>
+            <span className="anime-details-ids">Synopsis:</span>
+            <span className="anime-details-values">
+              {anime.synopsis ? anime.synopsis : <span className="unknown-details">unknown</span>}
+            </span>
+          </p>
+          <p>
+            <span className="anime-details-ids">Background:</span>
+            <span className="anime-details-values">
+              {anime.background ? anime.background : <span className="unknown-details">unknown</span>}
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
