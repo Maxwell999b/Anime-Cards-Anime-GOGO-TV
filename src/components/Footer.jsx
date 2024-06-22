461cb86b27f5413c3145e2121483596cea3ec5f5
Abdulrahman Ahmed <bedo150@gmail.com>
2024-06-22 09:34:38 +0300

    update Footer.jsx, Navbar.css and src/index.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import Github from "../assets/github-square-brands.svg";
 import Icon from "../assets/pnga2.png";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <img className="icon-sizing" src={Icon} alt="Icon" />
+      <Link to="/">
+        <img className="icon-sizing" src={Icon} alt="Icon" />
+      </Link>
 
       <div className="footer--copyright">
         <p>
-          &copy;2024 -<span style={{ color: "red" }}>Anime GOGO TV.</span> All rights reserved.
+          &copy;2024 -<span style={{ color: "red" }}>Anime GOGO TV.</span>{" "}
+          <span className="footer-hidden-small">All rights reserved. </span>
         </p>
       </div>
       <a className="footer--links" href="https://github.com/Maxwell999b" target="_blank" rel="noreferrer">
