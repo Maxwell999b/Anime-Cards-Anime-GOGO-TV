bc9a6fcb1666571ca7d00853ff493f782fc9e699
Abdulrahman Ahmed <bedo150@gmail.com>
2024-06-29 21:54:28 +0300

    docs for the api rate

--- src/components/services/http.jsx
+++ src/components/services/http.jsx
@@ -1,6 +1,12 @@
 import axios from "axios";
 import rateLimit from "axios-rate-limit";
 import axiosRetry from "axios-retry";
+// this based on this Rate Limiting
+// Duration	Requests
+// Daily	Unlimited
+// Per Minute	60 requests
+// Per Second	3 requests
+// Note: It's still possible to get rate limited from MyAnimeList.net instead.
 
 const http = rateLimit(axios.create(), {
   maxRequests: 60,
